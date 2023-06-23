5b43e123b1d8b6e11f4a0628b209cafedb2b7dd5
Jhon Erik <javilaortiz@gmail.com>
2023-06-23 05:53:33 +0000

    feat: defining places for components

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>React + Vite</h1>
-      <h2>On CodeSandbox!</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR.
-        </p>
-
-        <p>
-          Tip: you can use the inspector button next to address bar to click on
-          components in the preview and open the code in the editor!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="flex flex-col h-screen">
+      <header className="bg-gray-200 p-4">
+        Header
+        {/* Header content */}
+      </header>
+      <main className="flex-grow p-4">
+        Content
+        {/* Content */}
+      </main>
     </div>
   );
-}
+};
 
 export default App;
+
