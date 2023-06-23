1cde41fc42381e702ca53976540c9aa224d01f0d
Jhon Erik <javilaortiz@gmail.com>
2023-06-23 08:18:36 +0000

    feat: content banner

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import modusLogo from '../assets/modus-logo.png';
-import "../styles/Header.scss";
+import modusLogo from '../assets/tp-logo-modus.png';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-gray-200 p-4">
+    <header className="bg-white p-4">
       <div className="flex items-center justify-between">
         <img src={modusLogo} alt="Modus Logo" className="h-8" />
+        <input type="text" placeholder='Type your search here' />
+        <span className='font-bold'>STORE</span>
       </div>
     </header>
   );
