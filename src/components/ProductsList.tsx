16097da4d26c3ca8c81ab8e698410155572c92c5
Jhon Avila <javilaortiz@gmail.com>
2023-06-24 15:07:25 -0500

    fix: fixing codesmells on ts structure

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -5,7 +5,7 @@ import ProductItem from "./ProductItem";
 import ProductDetail from "./ProductDetail";
 
 interface ProductsListProps {
-  data: Product[];
+  data: Product[] | undefined;
   setImageParam: React.Dispatch<React.SetStateAction<string>>;
 }
 
@@ -22,7 +22,7 @@ const ProductsList: React.FC<ProductsListProps> = ({ data, setImageParam } ) => 
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-6">
-      {data.map((product) => (<div key={product.id} onClick={()=>setProduct(product)}><ProductItem product={product} handlerClick={handlerClick} /></div>) )}
+      {data && data.map((product) => (<div key={product.id} onClick={()=>setProduct(product)}><ProductItem product={product} handlerClick={handlerClick} /></div>) )}
       { product !== null && <ProductDetail product={product} onClose={handleClose} />}
     </div>
   );
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,12 @@
+import { QueryFunction } from 'react-query';
 import axios, {AxiosInstance, AxiosResponse} from 'axios';
+import { Product } from '../utils/entities';
 
 const API_URL: string = import.meta.env.VITE_API_URL;
 
 const api: AxiosInstance = axios.create();
 
-export const getProducts = async (): Promise<AxiosResponse | unknown> => {
+export const getProducts: QueryFunction<Product[]> = async () => {
   try {
     const response: AxiosResponse = await api.get(API_URL);
     return response.data.splice(0, 8);
