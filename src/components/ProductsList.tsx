18dc12ac3357edeeae77803e3431b5f85692634d
Jhon Erik <javilaortiz@gmail.com>
2023-06-24 02:52:01 -0500

    feat: service layer completed using react-query (#4)

---
+++ src/components/ProductsList.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Product } from "../utils/entities";
+import ProductItem from "./ProductItem";
+
+interface ProductsListProps {
+  data: Product[];
+  setImageParam: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const ProductsList: React.FC<ProductsListProps> = ({ data, setImageParam } ) => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 my-6">
+      {data.map((product) => (<div key={product.id}><ProductItem product={product} setImageParam={setImageParam} /></div>) )}
+    </div>
+  );
+}
+
+export default ProductsList;
--- src/containers/Content.tsx
+++ src/containers/Content.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+
 import Banner from '../components/Banner';
+import ProductsList from '../components/ProductsList';
+
+import { getProducts } from '../services/api';
+import loading from '../assets/loading.gif';
+import { Product } from '../utils/entities';
 
 const Content: React.FC = () => {
   const [imageParam, setImageParam] = React.useState<string>("store");
-  return <Banner imageParam={imageParam} />
+  const { data, isLoading, isError, error } = useQuery<Product[], any>('products', getProducts);
+
+  const content = (
+    <div className='w-full align-middle'>
+      { isLoading ? <img src={loading} alt="image" /> : <ProductsList data={data} setImageParam={setImageParam} /> }
+    </div>
+  )
+
+  return (
+    <>
+      <Banner imageParam={imageParam} />
+      <div className="container mx-auto px-6">
+        { content }
+      </div>
+    </>
+  );
 }
 
 export default Content;
